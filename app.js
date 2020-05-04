a09144c11e30b0c8f1cc831e802d82af09293827
sarah elhwarii <sarah.elhwarii92@gmail.com>
2020-05-04 04:35:20 +0300

    finished

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const randomNumber2 = Math.floor(Math.random() * 6 + 1);
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
 
-img1.setAttribute("src", `images/dice{randomNumber1}.png`);
-img2.setAttribute("src", `images/dice{randomNumber2}.png`);
-
-if (randomNumber1 > randomNumber2) {
+img1.setAttribute("src", `images/dice${randomNumber1}.png`);
+img2.setAttribute("src", `images/dice${randomNumber2}.png`);
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").textContent = "Draw";
+} else if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").textContent = "player 1,You won!!";
+  document.querySelector("h1").classList.add("new");
 } else {
   document.querySelector("h1").textContent = "player 2,You won!!";
+  document.querySelector("h1").classList.add("new");
 }
